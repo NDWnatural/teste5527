f7177f05ea36a26bde2ab1d71f0e79de04d7af8a
NDWnatural <134720252+NDWnatural@users.noreply.github.com>
2023-11-14 08:46:05 -0300

    Teu cuzinho negro

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var nome = prompt ('qual seu nome?')
 var data = new Date()
 var hora = data.getHours()
 var min = data.getMinutes()
-if (hora >0 && hora < 12){
+if (hora > 0 && hora < 12){
     alert('bom dia, ${nome}! agora são ${hora} horas e ${min} minutos...')
-} else if (hora <18){
+} else if (hora < 18){
     alert('boa tarde, ${nome}! agora são ${hora} horas e ${min} minutos')
 }
